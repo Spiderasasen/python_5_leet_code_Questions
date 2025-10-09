9156b35e53b6e74670566ec629fd3ebbadf99ad8
spiderasasen <spiderasasen@gmail.com>
2025-10-09 18:03:00 -0400

    Working on the roman int problam now working on Roman Intger Problem

--- question/easy_questions/Roman_Integer_Problem.py
+++ question/easy_questions/Roman_Integer_Problem.py
@@ -1,5 +1,15 @@
 import Reading_text_files as read
 
+#showing the player the requirements for the code
 def question():
     read.read_text_file('Text_Files_for_Questions/Roman_int.txt')
 
+class Solution(object):
+    #TODO make this shit possible
+    def romanToInt(self, s):
+        pass
+
+def solutions():
+    Solution().romanToInt('I') #number 1
+    Solution().romanToInt('III') #number 3
+    Solution().romanToInt('IV') # number 4
--- texting.py
+++ texting.py
@@ -1,7 +1,8 @@
 import question.easy_questions.Roman_Integer_Problem as Roman
 
 def main():
-    Roman.question()
+    # Roman.question()
+    Roman.solutions()
 
 if __name__ == '__main__':
     main()
