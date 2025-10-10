2fc1c1193eb402fea736b48088b59a677b6d07dc
spiderasasen <spiderasasen@gmail.com>
2025-10-10 08:57:02 -0400

    Working on the roman int problam Making the code and also a reader to read all the code

--- texting.py
+++ texting.py
@@ -2,7 +2,8 @@ import question.easy_questions.Roman_Integer_Problem as Roman
 
 def main():
     # Roman.question()
-    Roman.solutions()
+    # Roman.solutions()
+    Roman.read_code()
 
 if __name__ == '__main__':
     main()
