44073129d3d09d55184d7041ddf4e14cef6269de
spiderasasen <spiderasasen@gmail.com>
2025-10-14 10:27:56 -0400

    Working on the common letter array done with the code i used ai at the ed beacuse i had no idea what i was doing wrong i failed

--- texting.py
+++ texting.py
@@ -1,9 +1,12 @@
-import question.easy_questions.Roman_Integer_Problem as Roman
+import question.easy_questions.Common_letters_in_Array as Common
 
 def main():
     # Roman.question()
     # Roman.solutions()
-    Roman.read_code()
+    # Roman.read_code()
+    # Common.reading_question()
+    # Common.solutions()
+    Common.reading_code()
 
 if __name__ == '__main__':
     main()
